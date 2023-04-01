62604a42efe9476fa2d1349b173620bd21c053e9
b <b@DESKTOP-A3RKR60>
2023-03-31 22:45:03 -0500

    fix weird spacing?

---
+++ src/camera.h
@@ -0,0 +1,6 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -17,83 +17,88 @@
 
 
 const char* fragment_shader =
-        "#version 330 core\n"
-        "in vec3 color;\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "    FragColor = vec4(color, 1.0f);\n"
-        "}\n";
+	"#version 330 core\n"
+	"in vec3 color;\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"    FragColor = vec4(color, 1.0f);\n"
+	"}\n";
 
 const char* vertex_shader =
-        "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "layout (location = 1) in vec3 aColor;\n"
-        "out vec3 color;\n"
-        "void main()\n"
-        "{\n"
-        "    gl_Position = vec4(aPos, 1.0);\n"
-        "    color = aColor;\n"
-        "}\n";
+	"#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"out vec3 color;\n"
+	"uniform mat4 view;\n"
+	"uniform mat4 projection;\n"
+	"void main()\n"
+	"{\n"
+	"    gl_Position = projection * view *vec4(aPos, 1.0);\n"
+	"    color = aColor;\n"
+	"}\n";
 
 
 int main(void) {
     GLFWwindow* window;
 
-    // Initialize GLFW
     if (!glfwInit()) {
         printf("Failed to initialize GLFW\n");
         return -1;
     }
-
-    // Create a windowed mode window and its OpenGL context
-    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Simple Triangle", NULL, NULL);
+    
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "yo", NULL, NULL);
     if (!window) {
         printf("Failed to create GLFW window\n");
         glfwTerminate();
         return -1;
     }
-
-    // Make the window's context current
+    
     glfwMakeContextCurrent(window);
-
-    // Initialize GLEW
+    
     if (glewInit() != GLEW_OK) {
         printf("Failed to initialize GLEW\n");
         glfwTerminate();
         return -1;
     }
 
-	
     GLuint vbo, color_vbo;
 	setup_vbo(vbo, color_vbo);
 
-	GLuint vao;
-	setup_vao(vao);
-    
+	GLuint vao;setup_vao(vao);
+
     Shader shader = shader_create(vertex_shader, fragment_shader);
-   
-		
+
+    // Define camera parameters
+    vec3 camera_pos = {0.0f, 0.0f, 3.0f};
+    vec3 camera_target = {0.0f, 0.0f, 0.0f};
+    vec3 camera_up = {0.0f, 1.0f, 0.0f};
+
+    // Calculate view matrix using camera parameters
+    mat4 view_matrix;
+    glm_lookat(camera_pos, camera_target, camera_up, view_matrix);
+
+    // Set up camera uniform in shader
+    GLuint view_location = glGetUniformLocation(shader.ID, "view_matrix");
+    glUniformMatrix4fv(view_location, 1, GL_FALSE, (float*)view_matrix);
+
     while (!glfwWindowShouldClose(window)) {
-   
+
         glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shader.ID);
+        glUseProgram(shader.ID);
         glBindVertexArray(vao);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        // Swap buffers and poll for events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // Clean up resources
-	shader_destroy(&shader);
+    shader_destroy(&shader);
     glDeleteBuffers(1, &vbo);
     glDeleteBuffers(1, &color_vbo);
     glDeleteVertexArrays(1, &vao);
 
-    // Terminate GLFW
     glfwTerminate();
     return 0;
 }
