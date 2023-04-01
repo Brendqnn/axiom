e4551cacc6fad995963c8207deb4289b732a55f1
Brendqnn <96856316+Brendqnn@users.noreply.github.com>
2023-03-31 23:23:44 -0500

    Update vbo.c
    spacing

--- src/gfx/vbo.c
+++ src/gfx/vbo.c
@@ -16,18 +16,18 @@ GLfloat colors[] = {
 
 void setup_vbo(GLuint vbo, GLuint color_vbo) {
 	
-	glGenBuffers(1, &vbo);
+    glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
     glEnableVertexAttribArray(0);
 
     glGenBuffers(1, &color_vbo);
     glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
     glEnableVertexAttribArray(1);
 
 }
