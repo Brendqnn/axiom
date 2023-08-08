1015cc678907baa5b6aafdd8d35dcd2194417349
Brendqnn <bbbird226@gmail.com>
2023-08-08 16:49:38 -0500

    buffer

--- src/model.h
+++ src/model.h
@@ -1,17 +1,1 @@
-#ifndef MODEL_H
-#define MODEL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "mesh.h"
-
-
-typedef struct Model {
-    Mesh *mesh;
-} Model;
-
-Model *load_model(const char* filepath);
-void draw_model(Model *model);
-void free_model(Model *model);
-
-#endif // MODEL_H
